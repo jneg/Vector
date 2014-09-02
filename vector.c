3c3a34ba5545f3c26378c035381dd860a96875c6
jneg <jneg@users.noreply.github.com>
2014-09-02 11:35:59 -0700

    Added errors, mem safety, allocation, and deallocation

--- vector.c
+++ vector.c
@@ -6,10 +6,14 @@
 
 vector *vec_init(size_t bs)
 {
-   vector *v = malloc(sizeof(*v) + 8*bs);
-   if(!v) exit(1);
+   vector *v = malloc(sizeof(*v));
+   if(!v)
+   {
+      perror("Failed to allocate initial vector");
+      exit(1);
+   }
 
-   v->cap = 8;
+   v->cap = 0;
    v->len = 0;
    v->bs = bs;
    return v;
@@ -17,12 +21,17 @@ vector *vec_init(size_t bs)
 
 void vec_push(vector *v, void *data)
 {
-   if(v->cap == v->len)
+   if(v->cap == 0 || v->cap%8 != 0 || v->cap == v->len)
    {
-      v->cap *= 2;
+      if(v->cap == 0 || v->cap%8 != 0) v->cap += (8-v->cap%8);
+      else v->cap *= 2;
 
       v = realloc(v, sizeof(*v) + v->cap*v->bs);
-      if(!v) exit(1);
+      if(!v)
+      {
+         perror("Failed to expand vector memory");
+         exit(1);
+      }
       memset(&v->data[v->len*v->bs], 0, (v->cap-v->len)*v->bs);
    }
 
@@ -44,6 +53,24 @@ void vec_del(vector *v, size_t index)
    memset(&v->data[index*v->bs], 0, v->bs);
 }
 
+void vec_shrink(vector *v, size_t newlen)
+{
+   if(newlen >= v->len)
+   {
+      perror("New length to shrink to was not less than vector's length");
+      exit(1);
+   }
+   memset(&v->data[newlen*v->bs], 0, (v->len-newlen)*v->bs);
+
+   v->cap = v->len = newlen;
+   v = realloc(v, sizeof(*v) + v->cap*v->bs);
+   if(!v)
+   {
+      perror("Failed to shrink vector memory");
+      exit(1);
+   }
+}
+
 size_t vec_get(vector *v, size_t index)
 {
    size_t ret;
@@ -51,13 +78,13 @@ size_t vec_get(vector *v, size_t index)
    return ret;
 }
 
-void vec_empty(vector *v)
+void vec_free(vector **v)
 {
-   memset(&v->data, 0, v->cap*v->bs);
-}
-
-void vec_free(vector *v)
-{
-   free(v);
-   if(!v) exit(1);
+   free(*v);
+   *v = NULL;
+   if(*v)
+   {
+      perror("Failed to free vector");
+      exit(1);
+   }
 }
--- vector.h
+++ vector.h
@@ -11,6 +11,6 @@ extern void vec_push(vector *v, void *data);
 extern void vec_pop(vector *v);
 extern void vec_ins(vector *v, size_t index, void *data);
 extern void vec_del(vector *v, size_t index);
+extern void vec_shrink(vector *v, size_t newlen);
 extern size_t vec_get(vector *v, size_t index);
-extern void vec_empty(vector *v);
-extern void vec_free(vector *v);
+extern void vec_free(vector **v);
