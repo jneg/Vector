f5ab8e74ac83a48bfc287993664227e12fb47f5a
j neg <javon.negahban@cox.net>
2014-09-05 00:51:00 -0700

    Refactored code; get functions; better errors

--- vector.c
+++ vector.c
@@ -4,18 +4,15 @@
 
 #include "vector.h"
 
-vector *vec_init(size_t bs)
+vector *vec_new(size_t bs)
 {
    vector *v = malloc(sizeof(*v));
-   if(!v)
-   {
-      perror("Failed to allocate initial vector");
-      exit(1);
-   }
+   if(!v) perror("Failed to allocate initial vector");
 
    v->cap = 0;
    v->len = 0;
    v->bs = bs;
+
    return v;
 }
 
@@ -27,64 +24,77 @@ void vec_push(vector *v, void *data)
       else v->cap *= 2;
 
       v = realloc(v, sizeof(*v) + v->cap*v->bs);
-      if(!v)
-      {
-         perror("Failed to expand vector memory");
-         exit(1);
-      }
-      memset(&v->data[v->len*v->bs], 0, (v->cap-v->len)*v->bs);
+      if(!v) perror("Failed to expand vector memory");
+      memset(v->data + v->len*v->bs, 0, (v->cap-v->len)*v->bs);
    }
 
-   memcpy(&v->data[(v->len++)*v->bs], data, v->bs);
+   memcpy(v->data + v->len++ * v->bs, data, v->bs);
 }
 
 void vec_pop(vector *v)
 {
-   memset(&v->data[(--v->len)*v->bs], 0, v->bs);
+   memset(v->data + --v->len * v->bs, 0, v->bs);
 }
 
 void vec_ins(vector *v, size_t index, void *data)
-{
-   memcpy(&v->data[index*v->bs], data, v->bs);
+{ 
+   memcpy(v->data + index*v->bs, data, v->bs);
 }
 
-void vec_del(vector *v, size_t index)
+void vec_erase(vector *v, size_t index)
 {
-   memset(&v->data[index*v->bs], 0, v->bs);
+   memset(v->data + index*v->bs, 0, v->bs);
 }
 
 void vec_shrink(vector *v, size_t newlen)
 {
    if(newlen >= v->len)
    {
-      perror("New length to shrink to was not less than vector's length");
-      exit(1);
+      perror("Can't shrink when the new length is larger than than vector's length");
+      return;
    }
-   memset(&v->data[newlen*v->bs], 0, (v->len-newlen)*v->bs);
+
+   memset(v->data + newlen*v->bs, 0, (v->len-newlen)*v->bs);
 
    v->cap = v->len = newlen;
    v = realloc(v, sizeof(*v) + v->cap*v->bs);
-   if(!v)
+   if(!v) perror("Failed to shrink vector");
+}
+
+char vec_char(vector *v, size_t index)
+{
+   return *(v->data + index*v->bs);
+}
+
+short vec_short(vector *v, size_t index)
+{
+   return *(short*)(v->data + index*v->bs);
+}
+
+int vec_int(vector *v, size_t index)
+{
+   return *(int*)(v->data + index*v->bs);
+}
+
+long vec_long(vector *v, size_t index)
+{
+   return *(long*)(v->data + index*v->bs);
+}
+
+double vec_double(vector *v, size_t index)
+{
+   return *(double*)(v->data + index*v->bs);
+}
+
+void vec_del(vector **v)
+{
+   if(!*v)
    {
-      perror("Failed to shrink vector memory");
-      exit(1);
+      perror("Failed to delete nulled vector");
+      return;
    }
-}
 
-size_t vec_get(vector *v, size_t index)
-{
-   size_t ret;
-   memcpy(&ret, &v->data[index*v->bs], v->bs);
-   return ret;
-}
-
-void vec_free(vector **v)
-{
    free(*v);
    *v = NULL;
-   if(*v)
-   {
-      perror("Failed to free vector");
-      exit(1);
-   }
+   if(*v) perror("Failed to null vector");
 }
