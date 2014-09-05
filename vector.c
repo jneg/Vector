ff2bc82aba14071f4607760f2afeb69d2206d7ca
j neg <javon.negahban@cox.net>
2014-09-05 11:08:05 -0700

    Sanitized all inputs

--- vector.c
+++ vector.c
@@ -6,95 +6,209 @@
 
 vector *vec_new(size_t bs)
 {
-   vector *v = malloc(sizeof *v);
-   if(!v) perror("Failed to allocate initial vector");
+    if(bs < 0 || bs > 8) {
+        perror("New: Invalid byte length");
+        exit(EXIT_FAILURE);
+    }
 
-   v->cap = 0;
-   v->len = 0;
-   v->bs = bs;
+    vector *v = malloc(sizeof *v);
+    if(!v) {
+        perror("Failed to allocate initial vector");
+        exit(EXIT_FAILURE);
+    }
 
-   return v;
+    v->cap = 0;
+    v->len = 0;
+    v->bs = bs;
+
+    return v;
 }
 
 void vec_push(vector *v, void *data)
 {
-   if(v->cap == 0 || v->cap%8 != 0 || v->cap == v->len)
-   {
-      if(v->cap == 0 || v->cap%8 != 0) v->cap += (8-v->cap%8);
-      else v->cap *= 2;
+    if(!v) {
+        perror("Push: null vector");
+        return;
+    }
+    
+    if(!data) {
+        perror("Push: null data");
+        return;
+    }
 
-      v = realloc(v, sizeof *v + v->cap*v->bs);
-      if(!v) perror("Failed to expand vector memory");
-      memset(v->data + v->len*v->bs, 0, (v->cap-v->len)*v->bs);
-   }
+    if(v->cap == 0 || v->cap%8 != 0 || v->cap == v->len) {
+        if(v->cap == 0 || v->cap%8 != 0) {
+            v->cap += (8-v->cap%8);
+        } else {
+            v->cap *= 2;
+        }
 
-   memcpy(v->data + v->len++ * v->bs, data, v->bs);
+        v = realloc(v, sizeof *v + v->cap*v->bs);
+        if(!v) {
+            perror("Push: realloc failed");
+            return;
+        }
+
+        memset(v->data + v->len*v->bs, 0, (v->cap-v->len)*v->bs);
+    }
+
+    memcpy(v->data + v->len++ * v->bs, data, v->bs);
 }
 
 void vec_pop(vector *v)
 {
-   memset(v->data + --v->len * v->bs, 0, v->bs);
+    if(!v) {
+        perror("Pop: null vector");
+        return;
+    }
+
+    if(v->len == 0) {
+        perror("Pop: length already zero")
+        return;
+    }
+
+    memset(v->data + --v->len * v->bs, 0, v->bs);
 }
 
-void vec_ins(vector *v, size_t index, void *data)
+void vec_insert(vector *v, size_t index, void *data)
 { 
-   memcpy(v->data + index*v->bs, data, v->bs);
+    if(!v) {
+        perror("Insert: null vector");
+        return;
+    }
+
+    if(!data) {
+        perror("Insert: null data");
+        return;
+    }
+
+    if(index < 0 || index >= v->len) {
+        perror("Insert: index invalid");
+        return;
+    }
+
+    memcpy(v->data + index*v->bs, data, v->bs);
 }
 
 void vec_erase(vector *v, size_t index)
 {
-   memset(v->data + index*v->bs, 0, v->bs);
+    if(!v) {
+        perror("Erase: null vector");
+        return;
+    }
+
+    if(index < 0 || index >= v->len) {
+        perror("Erase: index invalid")
+        return;
+    }
+
+    memset(v->data + index*v->bs, 0, v->bs);
 }
 
 void vec_shrink(vector *v, size_t newlen)
 {
-   if(newlen >= v->len)
-   {
-      perror("Can't shrink when the new length is larger than than vector's length");
-      return;
-   }
+    if(!v) {
+        perror("Shrink: null vector");
+        return;
+    }
 
-   memset(v->data + newlen*v->bs, 0, (v->len-newlen)*v->bs);
+    if(newlen < 0 || newlen >=v->len) {
+        perror("Shrink: new length invalid");
+        return;
+    }
 
-   v->cap = v->len = newlen;
-   v = realloc(v, sizeof *v + v->cap*v->bs);
-   if(!v) perror("Failed to shrink vector");
+    memset(v->data + newlen*v->bs, 0, (v->len-newlen)*v->bs);
+
+    v->cap = v->len = newlen;
+    v = realloc(v, sizeof *v + v->cap*v->bs);
+    if(!v) {
+        perror("Shrink: realloc failed");
+        return;
+    }
 }
 
 char vec_char(vector *v, size_t index)
 {
-   return *(v->data + index*v->bs);
+    if(!v) {
+        perror("Char: null vector");
+        return '?';
+    }
+
+    if(index < 0 || index >= v->len) {
+        perror("Char: index invalid")
+        return '?';
+    }
+
+    return *(v->data + index*v->bs);
 }
 
 short vec_short(vector *v, size_t index)
 {
-   return *(short*)(v->data + index*v->bs);
+    if(!v) {
+        perror("Short: null vector");
+        return -1;
+    }
+
+    if(index < 0 || index >= v->len) {
+        perror("Short: index invalid")
+        return -1;
+    }
+
+    return *(short*)(v->data + index*v->bs);
 }
 
 int vec_int(vector *v, size_t index)
 {
-   return *(int*)(v->data + index*v->bs);
+    if(!v) {
+        perror("Int: null vector");
+        return -1;
+    }
+
+    if(index < 0 || index >= v->len) {
+        perror("Int: index invalid")
+        return -1;
+    }
+
+    return *(int*)(v->data + index*v->bs);
 }
 
 long vec_long(vector *v, size_t index)
 {
-   return *(long*)(v->data + index*v->bs);
+    if(!v) {
+        perror("Long: null vector");
+        return -1;
+    }
+
+    if(index < 0 || index >= v->len) {
+        perror("Long: index invalid")
+        return -1;
+    }
+
+    return *(long*)(v->data + index*v->bs);
 }
 
 double vec_double(vector *v, size_t index)
 {
-   return *(double*)(v->data + index*v->bs);
+    if(!v) {
+        perror("Double: null vector");
+        return -1.0;
+    }
+
+    if(index < 0 || index >= v->len) {
+        perror("Double: index invalid")
+        return -1.0;
+    }
+
+    return *(double*)(v->data + index*v->bs);
 }
 
 void vec_del(vector **v)
 {
-   if(!*v)
-   {
-      perror("Failed to delete nulled vector");
-      return;
-   }
+    if(!v || !*v) {
+        perror("Delete: null vector");
+        return;
+    }
 
-   free(*v);
-   *v = NULL;
-   if(*v) perror("Failed to null vector");
+    free(*v);
+    *v = NULL;
 }
--- vector.h
+++ vector.h
@@ -9,7 +9,7 @@ struct Vector
 extern vector *vec_new(size_t bs);
 extern void vec_push(vector *v, void *data);
 extern void vec_pop(vector *v);
-extern void vec_ins(vector *v, size_t index, void *data);
+extern void vec_insert(vector *v, size_t index, void *data);
 extern void vec_erase(vector *v, size_t index);
 extern void vec_shrink(vector *v, size_t newlen);
 extern char vec_char(vector *v, size_t index);
