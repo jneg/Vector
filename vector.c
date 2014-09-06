879c11904ed437539b35da3be5aa7a308ee61b73
j neg <javon.negahban@cox.net>
2014-09-06 11:54:24 -0700

    Added string errors, superior functions

--- vector.c
+++ vector.c
@@ -4,6 +4,30 @@
 
 #include "vector.h"
 
+// Note: I do not check for null pointers,
+// check for them yourself
+
+// Note: iterate through the whole vector using
+// for(it = vec_begin(v); it != vec_end(v); ++it)
+// where "it" is a pointer to the correct type
+
+// allocates more memory if necessary
+static int vec_alloc(vector **v)
+{
+    if((*v)->cap < 16 || (*v)->cap == (*v)->len) {
+        if((*v)->cap < 16)
+            (*v)->cap = 16;
+        else
+            (*v)->cap *= 2;
+
+        *v = realloc(*v, sizeof **v + (*v)->cap * (*v)->bs);
+        if(!*v)
+            return VEC_REALLOC;
+    }
+
+    return VEC_SUCCESS;
+}
+
 // mallocs vector and sets default values
 int vec_new(vector **v, size_t bs)
 {
@@ -18,78 +42,85 @@ int vec_new(vector **v, size_t bs)
 }
 
 // adds element to end and allocates memory if necessary
-int vec_push(vector *v, void *data)
+int vec_push(vector **v, void *data)
 {
-    if(v->cap == 0 || v->cap%8 != 0 || v->cap == v->len) {
-        if(v->cap == 0 || v->cap%8 != 0)
-            v->cap += (8-v->cap%8);
-        else
-            v->cap *= 2;
+    if(vec_alloc(v) == VEC_REALLOC)
+        return VEC_REALLOC;
 
-        v = realloc(v, sizeof *v + v->cap*v->bs);
-        if(!v)
-            return VEC_REALLOC;
-    }
-
-    memcpy(v->data + v->len++*v->bs, data, v->bs);
+    memcpy((*v)->data + (*v)->len++ * (*v)->bs, data, (*v)->bs);
 
     return VEC_SUCCESS;
 }
 
-// removes end element
-int vec_pop(vector *v)
+// removes last element
+int vec_pop(vector **v)
 {
-    if(v->len == 0)
+    if((*v)->len == 0)
         return VEC_INDEX;
 
-    --v->len;
+    --(*v)->len;
 
     return VEC_SUCCESS;
 }
 
 // overwrites element at index with data
-int vec_insert(vector *v, size_t index, void *data)
+int vec_replace(vector **v, size_t index, void *data)
 { 
-    if(index >= v->len)
+    if(index >= (*v)->len)
         return VEC_INDEX;
 
-    memcpy(v->data + index*v->bs, data, v->bs);
+    memcpy((*v)->data + index * (*v)->bs, data, (*v)->bs);
     
     return VEC_SUCCESS;
 }
 
-// removes element by shifting right elements on top of it
-int vec_erase(vector *v, size_t index)
+// shifts right elements and overwrites element at index
+int vec_insert(vector **v, size_t index, void *data)
 {
-    if(index >= v->len)
+    if(index >= (*v)->len)
         return VEC_INDEX;
 
-    memcpy(v->data + index*v->bs, v->data + (index + 1)*v->bs, (--v->len - index)*v->bs);
+    if(vec_alloc(v) == VEC_REALLOC)
+        return VEC_REALLOC;
+
+    memcpy((*v)->data + (index + 1) * (*v)->bs, (*v)->data + index * (*v)->bs, ((*v)->len++ - index) * (*v)->bs);
+    memcpy((*v)->data + index * (*v)->bs, data, (*v)->bs);
+
+    return VEC_SUCCESS;
+}
+
+// removes element by shifting right elements on top of it
+int vec_remove(vector **v, size_t index)
+{
+    if(index >= (*v)->len)
+        return VEC_INDEX;
+
+    memcpy((*v)->data + index * (*v)->bs, (*v)->data + (index + 1) * (*v)->bs, (--(*v)->len - index) * (*v)->bs);
     
     return VEC_SUCCESS;
 }
 
 // deallocates memory until new length
-int vec_shrink(vector *v, size_t newlen)
+int vec_shrink(vector **v, size_t newlen)
 {
-    if(newlen >= v->len)
+    if(newlen >= (*v)->len)
         return VEC_INDEX;
 
-    v->cap = v->len = newlen;
-    v = realloc(v, sizeof *v + v->cap*v->bs);
-    if(!v)
+    (*v)->cap = (*v)->len = newlen;
+    *v = realloc(*v, sizeof **v + (*v)->cap * (*v)->bs);
+    if(!*v)
         return VEC_REALLOC;
     
     return VEC_SUCCESS;
 }
 
 // copies element at index into get
-int vec_get(vector *v, size_t index, void *get)
+int vec_get(vector **v, size_t index, void *get)
 {
-    if(index >= v->len)
+    if(index >= (*v)->len)
         return VEC_INDEX;
 
-    memcpy(get, v->data + index*v->bs, v->bs);
+    memcpy(get, (*v)->data + index * (*v)->bs, (*v)->bs);
     
     return VEC_SUCCESS;
 }
@@ -103,7 +134,7 @@ void *vec_begin(vector *v)
 // returns iterator to the end of last element
 void *vec_end(vector *v)
 {
-    return v->data + v->len*v->bs;
+    return v->data + v->len * v->bs;
 }
 
 // frees vector
@@ -116,16 +147,18 @@ int vec_delete(vector **v)
 }
 
 // returns a string of the error code
+// enjoy your memory leaks
 const char *vec_error(int state)
 {
-    if(state == VEC_SUCCESS)
-        return "Success";
-    else if(state == VEC_MALLOC)
-        return "Vector Error: malloc unsuccessful";
-    else if(state == VEC_REALLOC)
-        return "Vector Error: realloc unsuccessful";
-    else if(state == VEC_INDEX)
-        return "Vector Error: index out of bounds";
-    else
-        return "State invalid";
+    if(state == VEC_SUCCESS) {
+        return strdup("Success");
+    } else if(state == VEC_MALLOC) {
+        return strdup("Failed to malloc vector!");
+    } else if(state == VEC_REALLOC) {
+        return strdup("Failed to realloc vector!");
+    } else if(state == VEC_INDEX) {
+        return strdup("Index out of bounds!");
+    } else {
+        return strdup("Invalid state");
+    }
 }
--- vector.h
+++ vector.h
@@ -19,12 +19,13 @@ struct Vector
 };
 
 int vec_new(vector **v, size_t bs);
-int vec_push(vector *v, void *data);
-int vec_pop(vector *v);
-int vec_insert(vector *v, size_t index, void *data);
-int vec_erase(vector *v, size_t index);
-int vec_shrink(vector *v, size_t newlen);
-int vec_get(vector *v, size_t index, void *get);
+int vec_push(vector **v, void *data);
+int vec_pop(vector **v);
+int vec_replace(vector **v, size_t index, void *data);
+int vec_insert(vector **v, size_t index, void *data);
+int vec_remove(vector **v, size_t index);
+int vec_shrink(vector **v, size_t newlen);
+int vec_get(vector **v, size_t index, void *get);
 void *vec_begin(vector *v);
 void *vec_end(vector *v);
 int vec_delete(vector **v);
